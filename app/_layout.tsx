c9dd5b8917a11ae344a8fa85d0bbf9229b251f81
Dave Gomez <davegomez426@gmail.com>
2025-12-01 12:38:18 -0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,53 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useColorScheme } from "@/hooks/use-color-scheme.web";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import "../global.css";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, "background");
+
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: "pink",
+          },
+          headerStyle: {
+            backgroundColor: "pink",
+          },
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Inicio" }} />s
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+const ComponentsApp = () => {
+  return (
+    <View>
+      <Text>ComponentsApp</Text>
+    </View>
+  );
+};
+export default ComponentsApp;
