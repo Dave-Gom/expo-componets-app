65b1d5b9329dc00c613ad361b995a5f124be80cd
Dave Gomez <davegomez426@gmail.com>
2025-12-16 16:45:05 -0300

    Refactor Animation101Screen to use custom useAnimation hook for improved code organization and reusability

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,39 +1,16 @@
+import useAnimation from "@/hooks/useAnimation";
 import ThemedButton from "@/presentation/shared/ThemedButton";
 import ThemedView from "@/presentation/shared/ThemedView";
-import { useRef } from "react";
 import { Animated } from "react-native";
 
 const Animation101Screen = () => {
-  const animatedOpacity = useRef(new Animated.Value(0)).current;
-  const animatedTop = useRef(new Animated.Value(-100)).current;
-
-  const fadeIn = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 1,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-
-    Animated.timing(animatedTop, {
-      toValue: 0,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    Animated.timing(animatedOpacity, {
-      toValue: 0,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-
-    Animated.timing(animatedTop, {
-      toValue: -100,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-  };
+  const {
+    animatedOpacity,
+    animatedTop,
+    fadeIn,
+    fadeOut,
+    startMovingTopPosition,
+  } = useAnimation();
 
   return (
     <ThemedView margin className="flex-1 justify-center items-center">
@@ -46,10 +23,16 @@ const Animation101Screen = () => {
           transform: [{ translateY: animatedTop }],
         }}
       />
-      <ThemedButton className="my-5" onPress={fadeIn}>
+      <ThemedButton
+        className="my-5"
+        onPress={() => {
+          fadeIn({});
+          startMovingTopPosition({});
+        }}
+      >
         Fade in
       </ThemedButton>
-      <ThemedButton className="my-5" onPress={fadeOut}>
+      <ThemedButton className="my-5" onPress={() => fadeOut({})}>
         Fade out
       </ThemedButton>
     </ThemedView>
