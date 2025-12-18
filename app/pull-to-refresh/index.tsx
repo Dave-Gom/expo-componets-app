b9426863f0e6d1c53b64b08d3106f776027f1e54
Dave Gomez <davegomez426@gmail.com>
2025-12-18 12:10:19 -0300

    Refactor PullToRefreshScreen to use themed components and implement pull-to-refresh functionality

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native';
+import { useThemeColor } from "@/hooks/use-theme-color";
+import ThemedText from "@/presentation/shared/ThemedText";
+import { useState } from "react";
+import { RefreshControl, ScrollView } from "react-native";
 
 const PullToRefreshScreen = () => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const backgroundColor = useThemeColor({}, "background");
+  const primaryColor = useThemeColor(
+    {
+      dark: "black",
+      light: "white",
+    },
+    "primary"
+  );
+
+  const onRefresh = () => {
+    setIsRefreshing(true);
+    // Simulate a network request
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 2000);
+  };
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+      }
+    >
+      <ScrollView>
+        <ThemedText>PullToRefreshScreen</ThemedText>
+      </ScrollView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
