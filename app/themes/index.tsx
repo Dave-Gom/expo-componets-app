e21ce43ce6f70e21e51e69e7c8da5c74ed139c4f
Dave Gomez <davegomez426@gmail.com>
2025-12-22 11:47:36 -0300

    Implement ThemesScreen with themed components and dark/system mode functionality

--- app/themes/index.tsx
+++ app/themes/index.tsx
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native';
+import ThemedCard from "@/presentation/shared/ThemedCard";
+import ThemedSwitch from "@/presentation/shared/ThemedSwitch";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { useColorScheme } from "nativewind";
+import { useState } from "react";
 
 const ThemesScreen = () => {
+  const { colorScheme, setColorScheme } = useColorScheme();
+  const [darkModeSetting, setDarkModeSetting] = useState({
+    darkMode: colorScheme === "dark",
+    systemMode: false,
+  });
+
+  const setDarkMode = (value: boolean) => {
+    setColorScheme(value ? "dark" : "light");
+    setDarkModeSetting({
+      darkMode: value,
+      systemMode: false,
+    });
+  };
+
+  const setSystemMode = (value: boolean) => {
+    setColorScheme("system");
+    setDarkModeSetting({
+      darkMode: colorScheme === "dark",
+      systemMode: value,
+    });
+  };
   return (
-    <View>
-      <Text>ThemesScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className="mt-5">
+        <ThemedSwitch
+          text="Dark Mode"
+          className="mb-5"
+          value={darkModeSetting.darkMode}
+          onValueChange={setDarkMode}
+        />
+        <ThemedSwitch
+          text="System Mode"
+          className="mb-5"
+          value={darkModeSetting.systemMode}
+          onValueChange={setSystemMode}
+        />
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default ThemesScreen;
